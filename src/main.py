4af814a0a384d3eda9d6008dd06e71cfe5ff940b
k0killette <c.jalouneix@gmail.com>
2025-05-26 15:21:04 +0200

    implementation couche presentation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .config import settings
+from .api.routes import api_router
+from .models import base, books, users, loans  # Importer les modèles pour Alembic
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -17,6 +19,9 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+# Inclusion des routes API
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 @app.get("/")
 def read_root():
